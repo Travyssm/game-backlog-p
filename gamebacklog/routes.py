c70accea344525b168a6bfc7de9e62a27b976f54
Travyssm <travys.mills@hotmail.com>
2024-09-27 16:58:29 +0000

    created add_genre.html and updated routes

--- gamebacklog/routes.py
+++ gamebacklog/routes.py
@@ -1,7 +1,21 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from gamebacklog import app, db
 from gamebacklog.models import Genre, Game
 
 @app.route("/")
 def home():
     return render_template("games.html")
+
+
+@app.route("/genres")
+def genres():
+    return render_template("genres.html")
+
+@app.route("/add_genre", methods=["GET", "POST"])
+def add_genre():
+    if request.method == "POST":
+        genre = Genre(genre_name=request.form.get("genre_name"))
+        db.session.add(genre)
+        db.session.commit()
+        return redirect(url_for("genres"))
+    return render_template("add_genre.html")
